59862b3b1bee09a8bc5eb4b42f03536b0be3e4e2
Osvaldss <zadeikisosvaldas@gmail.com>
2021-10-17 10:38:45 +0000

    Add main page and register form

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists('env.py'):
     import env
 
@@ -20,6 +21,13 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+@app.route("/get_latest")
+def get_latest():
+    '''Find recent added recipes and display then on home page'''
+    recipes = mongo.db.recipes.find()
+    return render_template("main_page.html", recipes=recipes)
+
+
 @app.route("/get_recipe")
 def get_recipe():
     '''Testing flask '''
@@ -27,11 +35,9 @@ def get_recipe():
     return render_template("recipes.html", recipes=recipes)
 
 
-@app.route("/get_latest")
-def get_latest():
-    '''Find recent added recipes and display then on home page'''
-    recipes = mongo.db.recipes.find()
-    return render_template("main_page.html", recipes=recipes)
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
