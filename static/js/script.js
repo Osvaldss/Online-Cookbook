fc67cad533b12bd8860c0052da979780029c221b
Osvaldss <zadeikisosvaldas@gmail.com>
2021-10-19 10:39:58 +0000

    Add data from mongodb to recipes page and add custom styling with responsive script

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    checkWidth();
+    $(window).resize(checkWidth);
   });
+
+function checkWidth() {
+  var detImgCont = $(".image-container").detach();
+
+  if($(window).width() < 600) { 
+    detImgCont;
+    $(".card-content.short-desc").prepend(detImgCont);
+  } else {
+    detImgCont;
+    $(".rm-bottom-gap").append(detImgCont);
+  }
+};
