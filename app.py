dba4cd9f999e69ea933e9473def5ec5bfef06320
Osvaldss <zadeikisosvaldas@gmail.com>
2021-12-23 11:32:56 +0000

    Changed debug value on app.py

--- app.py
+++ app.py
@@ -453,4 +453,4 @@ def error404(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
